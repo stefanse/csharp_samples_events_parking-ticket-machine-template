3a757671e57dce1d2e721c1322165bd5c19b4f82
stefanse <s.steininger@hotmail.com>
2020-01-16 20:13:03 +0100

    initializing Project

--- source/ParkingTicketMachine.Wpf/MainWindow.xaml.cs
+++ source/ParkingTicketMachine.Wpf/MainWindow.xaml.cs
@@ -15,13 +15,23 @@ namespace ParkingTicketMachine.Wpf
             InitializeComponent();
         }
 
-        private void Window_Initialized(object sender, EventArgs e)
+        private void MetroWindow_Loaded(object sender, EventArgs e)
         {
+            //einsprugnspunkt bei
+            FastClock.Instance.Factor = 360;
+            FastClock.Instance.Time = DateTime.Now;
+            FastClock.Instance.OneMinuteIsOver += Instance_OneMinuteIsOver;
+            FastClock.Instance.IsRunning = true;
+        }
+
+        private void Instance_OneMinuteIsOver(object sender, DateTime e)
+        {
+            this.Title = e.ToShortTimeString();
         }
 
         private void ButtonNew_Click(object sender, RoutedEventArgs e)
         {
-            
+            SlotMachineWindow newWindow = new SlotMachineWindow(TextBoxAddress.Text, ButtonPrintTicket_Click); // Methode die aufgerufen wird wenn auf Ticket gedrückt
         }
 
     }
